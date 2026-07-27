e5cbc8fc659a54c67570891bf0cdf7fd81344148
Wallace Souza <wallacewinner@gmail.com>
2026-07-26 21:48:09 -0300

    feat: implement JWT token splitting and base64 decoding logic for console output

--- jwt_decrypt/index.js
+++ jwt_decrypt/index.js
@@ -2,7 +2,20 @@ const fs = require('node:fs');
 
 function main() {
     console.log('jwt_decrypt');
-    const [, , inputFile] = process.argv;
+    const [, , jwtToken] = process.argv;
+    console.log(jwtToken);
+    const [header, payload, signature] = splitToken(jwtToken);
+    console.log('Header: ', base64Decode(header));
+    console.log('Payload: ', base64Decode(payload));
+    console.log('Signature: ', base64Decode(signature));
+}
+
+function splitToken(jwtToken) {
+    return jwtToken.split('.');
+}
+
+function base64Decode(base64String) {
+    return Buffer.from(base64String, 'base64').toString('utf-8');
 }
 
 main();
