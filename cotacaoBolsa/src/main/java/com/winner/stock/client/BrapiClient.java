59667c4b90ce93815f321f403ebabeb27ea21004
Wallace Souza <wallacewinner@gmail.com>
2026-09-11 22:02:41 -0300

    feat: add Brapi Feign client and StockService for stock quotes

---
+++ cotacaoBolsa/src/main/java/com/winner/stock/client/BrapiClient.java
@@ -0,0 +1,12 @@
+package com.winner.stock.client;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.cloud.openfeign.FeignClient;
+import com.winner.stock.dto.BrapiResponseDto;
+
+@FeignClient(url = "https://brapi.dev", name = "brapi")
+public interface BrapiClient {
+    @GetMapping("/api/v2/stocks/{symbol}")
+        BrapiResponseDto getStockQuote(@PathVariable("symbol") String symbol);
+}
---
+++ cotacaoBolsa/src/main/java/com/winner/stock/service/StockService.java
@@ -0,0 +1,18 @@
+package com.winner.stock.service;
+
+import com.winner.stock.client.BrapiClient;
+import com.winner.stock.dto.BrapiResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StockService {
+   private final BrapiClient apiClient;
+
+   public StockService(BrapiClient apiClient) {
+      this.apiClient = apiClient;
+   }
+
+   public BrapiResponseDto getStockQuote(String symbol) {
+      return apiClient.getStockQuote(symbol);
+   }
+}
